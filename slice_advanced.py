4ad93d9963f9a0873d8e5892558380a9d5866b16
juansebas06 <162723503+juansebas06@users.noreply.github.com>
2024-04-11 23:51:27 -0600

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,5 +1,10 @@
 def slice_advanced():
-    # Código a implementar utilizando input.
+    name=f'{input('texto = ').lower()}'
+    print(name[:3])
+    name1=len(name)
+    print(name[name1//2 - 1:name1 // 2 + 2])
+    print(f'{name[:4]}{name[-3:]}')
+
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
