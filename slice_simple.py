474dce3afe2213405a0d93662e06e086b369ca17
juansebas06 <162723503+juansebas06@users.noreply.github.com>
2024-04-11 23:55:00 -0600

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,5 +1,11 @@
 def slice_simple():
     texto = "Awesome"
+    
+    print(texto[:3])
+    name1=len(texto)
+    print(name[name1//2 - 1:name1 // 2 + 2])
+    print(f'{texto[:4]}{texto[-3:]}')
+
     # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
     # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
 
