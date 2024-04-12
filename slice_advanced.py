149fb9c38baca12bd0940c6da88e3259cb56abaf
juansebas06 <162723503+juansebas06@users.noreply.github.com>
2024-04-11 23:55:41 -0600

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,9 +1,5 @@
 def slice_advanced():
-    name=f'{input('texto = ').lower()}'
-    print(name[:3])
-    name1=len(name)
-    print(name[name1//2 - 1:name1 // 2 + 2])
-    print(f'{name[:4]}{name[-3:]}')
+    
 
 
 # Para verificar este ejercicio ejecutar el comando
