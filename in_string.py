c162fe6b1abf69168da0cfbc18e4843a9cf27378
juansebas06 <162723503+juansebas06@users.noreply.github.com>
2024-04-11 21:40:00 -0600

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,12 +1,12 @@
 def check_vowels():
    nombre=f"{input("ingrese palabra:").lower()}"
-a= "a" in nombre
-b= "e" in nombre
-c= "i" in nombre
-d= "o" in nombre
-e= "u" in nombre
-print(f"contiene a: {a}")
-print(f"contiene e: {b}")
-print(f"contiene i: {c}")
-print(f"contiene o: {d}")
-print(f"contiene u: {e}")
+   a= "a" in nombre
+   b= "e" in nombre
+   c= "i" in nombre
+   d= "o" in nombre
+   e= "u" in nombre
+   print(f"contiene a: {a}")
+   print(f"contiene e: {b}")
+   print(f"contiene i: {c}")
+   print(f"contiene o: {d}")
+   print(f"contiene u: {e}")
